247c4710b584977d417387722d6de35ae136d0b2
FarbodPM <farbod.peimanzadeh@gmail.com>
2022-09-09 22:11:34 +0430

    fix auto accept apt

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
-sudo apt-get update
-sudo apt-get install \
+sudo apt-get update -y
+sudo apt-get install -y \
     ca-certificates \
     curl \
     gnupg \
@@ -9,5 +9,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
